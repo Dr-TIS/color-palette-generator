5d6d5c7d5eb68f01095d66654678b4e62107a1ab
Bruno Barreto <bruno@brunobarreto.net>
2020-07-03 16:24:46 -0300

    Updating libs and removing deprecated babel

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# color-palette-generator [![Build Status](https://travis-ci.org/tiborv/color-palette-generator.svg?branch=master)](https://travis-ci.org/tiborv/color-palette-generator) [![Greenkeeper badge](https://badges.greenkeeper.io/tiborv/color-palette-generator.svg?token=e2196b0ecb06015f7be45d5099b0aeaafb17ea7e477a202cc6b474559136f141&ts=1496667265590)](https://greenkeeper.io/)
+# color-palette-generator [![Build Status](https://travis-ci.org/Dr-TIS/color-palette-generator.svg?branch=master)](https://travis-ci.org/Dr-TIS/color-palette-generator)
 
 ## Example
+
 ```js
-const colorGen = require('color-palette-generator') // colorGen(baseColor, numberOfColorsToGenerate, mixOutput)
+const colorGen = require("color-palette-generator"); // colorGen(baseColor, numberOfColorsToGenerate, mixOutput)
 
-console.log(colorGen('#D60000', 12)) // [ '#D60000', '#D64700', '#D68F00', '#D6D600', '#8FD600', '#47D600', '#00D600', '#00D647', '#00D68F', '#00D6D6', '#008FD6', '#0047D6' ]
+console.log(colorGen("#D60000", 12)); // [ '#D60000', '#D64700', '#D68F00', '#D6D600', '#8FD600', '#47D600', '#00D600', '#00D647', '#00D68F', '#00D6D6', '#008FD6', '#0047D6' ]
 
-console.log(colorGen('#D60000', 12, true)) // [ '#D60000', '#00D600', '#D64700', '#00D647', '#D68F00', '#00D68F', '#D6D600', '#00D6D6', '#8FD600', '#008FD6', '#47D600', '#0047D6' ]
+console.log(colorGen("#D60000", 12, true)); // [ '#D60000', '#00D600', '#D64700', '#00D647', '#D68F00', '#00D68F', '#D6D600', '#00D6D6', '#8FD600', '#008FD6', '#47D600', '#0047D6' ]
 ```
-
--- test/test.js
+++ test/test.js
@@ -1,42 +1,42 @@
-const colorGen = require('../')
-const { test } = require('ava')
+const colorGen = require("../");
+const test = require("ava");
 
-test('test standard', t => {
+test("test standard", (t) => {
   const colors = [
-    '#D60000',
-    '#D64700',
-    '#D68F00',
-    '#D6D600',
-    '#8FD600',
-    '#47D600',
-    '#00D600',
-    '#00D647',
-    '#00D68F',
-    '#00D6D6',
-    '#008FD6',
-    '#0047D6'
-  ]
-  const genColors = colorGen('#d60000', 12)
-  t.deepEqual(colors, genColors)
-  t.is(colors.length, genColors.length)
-})
+    "#D60000",
+    "#D64700",
+    "#D68F00",
+    "#D6D600",
+    "#8FD600",
+    "#47D600",
+    "#00D600",
+    "#00D647",
+    "#00D68F",
+    "#00D6D6",
+    "#008FD6",
+    "#0047D6",
+  ];
+  const genColors = colorGen("#d60000", 12);
+  t.deepEqual(colors, genColors);
+  t.is(colors.length, genColors.length);
+});
 
-test('test mix', t => {
+test("test mix", (t) => {
   const colors = [
-    '#D60000',
-    '#00D600',
-    '#D64700',
-    '#00D647',
-    '#D68F00',
-    '#00D68F',
-    '#D6D600',
-    '#00D6D6',
-    '#8FD600',
-    '#008FD6',
-    '#47D600',
-    '#0047D6'
-  ]
-  const genColors = colorGen('#d60000', 12, true)
-  t.deepEqual(colors, genColors)
-  t.is(colors.length, genColors.length)
-})
+    "#D60000",
+    "#00D600",
+    "#D64700",
+    "#00D647",
+    "#D68F00",
+    "#00D68F",
+    "#D6D600",
+    "#00D6D6",
+    "#8FD600",
+    "#008FD6",
+    "#47D600",
+    "#0047D6",
+  ];
+  const genColors = colorGen("#d60000", 12, true);
+  t.deepEqual(colors, genColors);
+  t.is(colors.length, genColors.length);
+});
